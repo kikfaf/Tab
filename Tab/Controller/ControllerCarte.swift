a22ece4810dada763021ceacf9c94eb00ff8519d
DAVID <fafa@me.com>
2020-03-06 08:50:50 +0100

    Release

--- Tab/Controller/ControllerCarte.swift
+++ Tab/Controller/ControllerCarte.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
 class ControllerCarte: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +23,19 @@ class ControllerCarte: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: mapView.mapType = MKMapType.standard
+        case 1: mapView.mapType = .satellite
+        case 2: mapView.mapType = .hybrid
+        default: break
+        }
+        
     }
-    */
+    
+    
+    @IBAction func getPosition(_ sender: Any) {
+    }
 
 }
